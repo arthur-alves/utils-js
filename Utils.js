9745b0fa37c010e2cad25f11db86455b6ce4790f
Arthur <arthur.4lves@gmail.com>
2014-07-25 18:52:54 -0300

    Update Utils.js

--- Utils.js
+++ Utils.js
@@ -1,5 +1,4 @@
-//retorna as variáveis da url
-function requestVars(){	
+//get parameters from url
 	var params = window.location.search
 	if (params){
 		var url_params = params.replace('?','').split('&');
@@ -13,3 +12,29 @@ function requestVars(){
 		return false;
 	};
 };
+
+
+// Preview of image-upload
+// API FILE html5
+function mostraImagem(files){
+	//Select element
+	div_img = document.getElementsByClassName('field-img_post');
+
+	//Get the position of object
+	var file = files[0];
+	var img = document.createElement("img");
+	img.setAttribute('style','margin:20px 0 0 180px; float:left;');
+	img.setAttribute('width','270');
+	img.setAttribute('width','160');
+	img.setAttribute('id','preview-img')
+	img.file = file;
+
+	div_img[0].appendChild(img)
+	// Mostra 
+	console.dir(file)
+	
+	// create a reader for file
+	var reader = new FileReader();
+	reader.onload = (function(aImg) {return function(e) {aImg.src = e.target.result;};})(img);
+	reader.readAsDataURL(file);
+}
