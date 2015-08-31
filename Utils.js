5b535bf1c6126020238c0b2d0169c63a641be214
Arthur <arthur.4lves@gmail.com>
2015-08-31 14:39:07 -0300

    Update Utils.js

--- Utils.js
+++ Utils.js
@@ -1,16 +1,17 @@
 //get parameters from url
-	var params = window.location.search
-	if (params){
-		var url_params = params.replace('?','').split('&');
-		var requests = {};
-		for(i in url_params){
-			var chave = url_params[i].split('=');
-			requests[chave[0]] =  chave[1];
-		}
-		return requests;	
-	}else{
-		return false;
-	};
+function requestVars(){ 
+    var params = window.location.search
+    if (params){
+        var url_params = params.replace('?','').split('&');
+        var requests = {};
+        for(i in url_params){
+            var chave = url_params[i].split('=');
+            requests[chave[0]] =  chave[1];
+        }
+        return requests;    
+    }else{
+        return false;
+    };
 };
 
 
